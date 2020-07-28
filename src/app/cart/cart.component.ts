21d4483fd886a0eff4f5e50307c22d4823a2d2ac
krishna malhotra <krishna.malhotra691692@gmail.com>
2020-07-28 13:52:26 +0530

    Added Sweet Alerts

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { CartService } from './../cart.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-cart',
@@ -68,7 +69,12 @@ export class CartComponent implements OnInit {
   }
 
   removeItem(foodId){
-    this.cartService.removeItem(foodId).subscribe(data => {
+    this.cartService.removeItem(foodId).subscribe(async data => {
+      await Swal.fire({
+          icon:'success',
+          title:'Item removed from cart',
+          timer:1000
+      })
       this.cartService.showCart().subscribe(data1 => {
         this.items = data1;
         let length = this.items.length;
@@ -86,8 +92,13 @@ export class CartComponent implements OnInit {
   }
 
   checkout(){
-    this.cartService.checkout().subscribe(data => {
-      alert('Order Placed Successfully!')
+    this.cartService.checkout().subscribe(async data => {
+      await Swal.fire({
+        icon:'success',
+        title:'Joy Of Happiness!',
+        text:'Order placed successfully..',
+        timer:1000
+      })
       this.router.navigate(['home']);
     })
   }
